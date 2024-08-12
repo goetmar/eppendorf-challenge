f3814f8aae76fbae72e1cddcbd31dd9c05b787ed
Marco <goettgens.marco@gmail.com>
2024-08-12 15:03:27 +0200

    refactor(e2e): create error message tests from function

--- tests/e2e/register-page.spec.ts
+++ tests/e2e/register-page.spec.ts
@@ -1,13 +1,32 @@
 import { test as base, expect, Page } from "@playwright/test";
 import { RegisterPage } from "../page-object-models/register-page";
 
-export const test = base.extend<{ registerPage: RegisterPage }>({
+const test = base.extend<{ registerPage: RegisterPage }>({
   registerPage: async ({ page }, use) => {
     const registerPage = new RegisterPage(page);
     await registerPage.goto();
     await use(registerPage);
   },
 });
+
+function composeErrorMessageTest(props: {
+  title: string;
+  name?: string;
+  email?: string;
+  password?: string;
+  expectedErrorMessage: string;
+}) {
+  test(props.title, async ({ registerPage }) => {
+    await registerPage.fillForm(
+      props.name || "",
+      props.email || "",
+      props.password || ""
+    );
+    await registerPage.submit();
+    const errorMessages = await registerPage.getErrorMessages();
+    expect(errorMessages).toContain(props.expectedErrorMessage);
+  });
+}
 
 test("show error messages for empty inputs", async ({ registerPage }) => {
   await registerPage.fillForm("", "", "");
@@ -20,67 +39,38 @@ test("show error messages for empty inputs", async ({ registerPage }) => {
   ]);
 });
 
-test("show error message for short name", async ({ registerPage }) => {
-  await registerPage.fillForm("a", "", "");
-  await registerPage.submit();
-  const errorMessages = await registerPage.getErrorMessages();
-  expect(errorMessages).toContain("Name must have at least two characters");
+composeErrorMessageTest({
+  title: "show error message for short name",
+  name: "a",
+  expectedErrorMessage: "Name must have at least two characters",
 });
-
-test("show error message for email without @ symbol", async ({
-  registerPage,
-}) => {
-  await registerPage.fillForm("", "email", "");
-  await registerPage.submit();
-  const errorMessages = await registerPage.getErrorMessages();
-  expect(errorMessages).toContain("Email must have an @ Symbol");
+composeErrorMessageTest({
+  title: "show error message for email without @ symbol",
+  email: "email",
+  expectedErrorMessage: "Email must have an @ Symbol",
 });
-
-test("show error message for invalid email format", async ({
-  registerPage,
-}) => {
-  await registerPage.fillForm("", "invalid@email", "");
-  await registerPage.submit();
-  const errorMessages = await registerPage.getErrorMessages();
-  expect(errorMessages).toContain("Email must be valid");
+composeErrorMessageTest({
+  title: "show error message for invalid email format",
+  email: "invalid@email",
+  expectedErrorMessage: "Email must be valid",
 });
-
-test("show error message for short password", async ({ registerPage }) => {
-  await registerPage.fillForm("", "", "abcdefg");
-  await registerPage.submit();
-  const errorMessages = await registerPage.getErrorMessages();
-  expect(errorMessages).toContain(
-    "Password must have at least eight characters"
-  );
+composeErrorMessageTest({
+  title: "show error message for short password",
+  password: "1234567",
+  expectedErrorMessage: "Password must have at least eight characters",
 });
-
-test("show error message for password without uppercase letter", async ({
-  registerPage,
-}) => {
-  await registerPage.fillForm("", "", "abcdefghi");
-  await registerPage.submit();
-  const errorMessages = await registerPage.getErrorMessages();
-  expect(errorMessages).toContain(
-    "Password must have at least one uppercase letter"
-  );
+composeErrorMessageTest({
+  title: "show error message for password without uppercase letter",
+  password: "abcdefghi",
+  expectedErrorMessage: "Password must have at least one uppercase letter",
 });
-
-test("show error message for password without special character", async ({
-  registerPage,
-}) => {
-  await registerPage.fillForm("", "", "ABCdefghi");
-  await registerPage.submit();
-  const errorMessages = await registerPage.getErrorMessages();
-  expect(errorMessages).toContain(
-    "Password must have at least one special character"
-  );
+composeErrorMessageTest({
+  title: "show error message for password without special character",
+  password: "ABCdefghi",
+  expectedErrorMessage: "Password must have at least one special character",
 });
-
-test("show error message for password without number", async ({
-  registerPage,
-}) => {
-  await registerPage.fillForm("", "", "ABCdefghi!");
-  await registerPage.submit();
-  const errorMessages = await registerPage.getErrorMessages();
-  expect(errorMessages).toContain("Password must have at least one number");
+composeErrorMessageTest({
+  title: "show error message for password without number",
+  password: "ABCdefghi!",
+  expectedErrorMessage: "Password must have at least one number",
 });
